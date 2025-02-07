bb1a4d67b73f65f1ee61c109521d7a63d4142d15
salamlam <hajri.slim1@gmail.com>
2025-02-07 14:59:36 +0100

    Update ReportTripView.dart

--- lib/screens/ReportTripView.dart
+++ lib/screens/ReportTripView.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:gpspro/arguments/ReportArguments.dart';
 import 'package:gpspro/model/PlayBackRoute.dart';
